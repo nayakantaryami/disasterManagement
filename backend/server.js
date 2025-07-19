aceffa6dfbb9ff58b0af63a99740764f64e4da46
nayakantaryami <nayakantaryami324@gmail.com>
2025-07-20 00:49:44 +0530

    added folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import "dotenv/config"
 const app=express();
-app.listen(4000,()=>{
-  console.log("app is listening");
-  
+
+const PORT=process.env.PORT||4000
+
+
+app.listen(PORT,()=>{
+  console.log(`app is listening on port ${PORT}`);
+
 });
